3416140139eb81c81dcba01f39a13294c0ddab1c
LayanZagha <lzagha@restaurant365.com>
2023-11-06 00:10:53 +0200

    Implement Product and Inventory classes and write the first function (AddProduct) in inventory class

---
+++ First Task-Simple Inventory Management System/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Task_Simple_Inventory_Management_System
+{
+    public class Inventory
+    {
+       public static List<Product> productsList = new List<Product>();
+
+        public void AddProduct(Product myProduct)
+        {
+            productsList.Add(myProduct);
+       
+        }
+
+    }
+}
--- First Task-Simple Inventory Management System/Program.cs
+++ First Task-Simple Inventory Management System/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using First_Task_Simple_Inventory_Management_System;
+
+Console.WriteLine("Please Enter The Product Name");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please Enter The Product Quantity");
+string quantityString = Console.ReadLine();
+int quantity = int.Parse(quantityString);
+
+Console.WriteLine("Please Enter The Product Price");
+string priceString = Console.ReadLine();
+int price = int.Parse(priceString);
+
+Product product = new Product(name, quantity, price);
+Inventory inventory = new Inventory();
+
+inventory.AddProduct(product);
